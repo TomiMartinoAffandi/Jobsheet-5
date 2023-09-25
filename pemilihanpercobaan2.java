72bfdde92e00ca38cc2343f7b5222ff322f6d709
TomiMartinoAffandi <tomimartino10@gmail.com>
2023-09-25 13:15:47 +0700

    pertanyaan percobaan 2

--- pemilihanpercobaan2.java
+++ pemilihanpercobaan2.java
@@ -14,8 +14,23 @@ public class pemilihanpercobaan2 {
 
         float total = (UAS * 0.4F)+(UTS * 0.35F)+(Kuis * 0.1F)+(Tugas * 0.2F);
 
-        String message = total < 65 ? "Remidi" : "Tidak Remidi";
+        String message = total <65 ?"remidi" : "tidak remidi";
 
-        System.out.println("Nilai akhir = " + total + "sehingga" + message);
+        if(total >80 && total <=100)
+            System.out.println("Nilai Huruf A, Nilai Setara 4, Kualifikasi Sangat Baik");
+        else if(total >73 && total <=80)
+            System.out.println("Nilai Huruf B+, Nilai Setara 3,5, Kulifikasi Lebih Dari Baik");
+        else if(total >65 && total <=73)
+            System.out.println("Nilai Huruf B, Nilai Setara 3, Kualifikasi Baik");
+        else if(total >60 && total <=65)
+            System.out.println("Nilai Huruf C+, Nilai Setara 2,5, Kualifikasi Lebih Dari Cukup");
+        else if(total >50 && total <=60)
+            System.out.println("Nilai Huruf C, Nilai Setara 2, Kualifikasi Cukup");
+        else if(total >39 && total <=50)
+            System.out.println("Nilai Huruf D, Nilai Setara 1, Kualifikasi Kurang");
+        else if(total <=39)
+            System.out.println("Nilai Huruf E, Nilai Setara 0, Kualifikasi Gagal");
+        
+        System.out.println("Nilai Akhir Anda " + total + " sehingga " + message);
     }
 }

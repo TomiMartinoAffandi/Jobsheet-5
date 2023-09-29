81e28b55fbfbb805de47e86ca08e758be6b6f031
TomiMartinoAffandi <tomimartino10@gmail.com>
2023-09-29 08:23:25 +0700

    Tugas P5

--- tugasP5.java
+++ tugasP5.java
@@ -1,36 +1,34 @@
 import java.util.Scanner;
-
 public class tugasP5 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-
-        // Langkah 1: Input
+        Scanner input14 = new Scanner(System.in);
+        
         System.out.print("Masukkan berat paket (kg): ");
-        double beratPaket = input.nextDouble();
-
-        System.out.print("Masukkan jarak pengiriman (km): ");
-        double jarakPengiriman = input.nextDouble();
-
+        double beratPaket = input14.nextDouble();
+        System.out.print("Masukkan jarak pengiriman(km): ");
+        double jarakPengiriman = input14.nextDouble();
         System.out.print("Masukkan jenis layanan (express/regular/standard): ");
-        String jenisLayanan = input.next();
-
-        // Langkah 2: Proses
-        double biayaPengiriman = 0.0;
-
-        if (jenisLayanan.equals("express")) {
-            biayaPengiriman = beratPaket * jarakPengiriman * 0.2; // Contoh perhitungan express
-        } else if (jenisLayanan.equals("regular")) {
-            biayaPengiriman = beratPaket * jarakPengiriman * 0.1; // Contoh perhitungan regular
-        } else if (jenisLayanan.equals("standard")) {
-            biayaPengiriman = beratPaket * jarakPengiriman * 0.05; // Contoh perhitungan standard
-        } else {
-            System.out.println("Jenis layanan tidak valid.");
-            System.exit(1); // Keluar dari program jika jenis layanan tidak valid
+        String jenisLayanan = input14.next();
+        
+        double biayaPengiriman,layanan = 0.0;
+        
+        switch (jenisLayanan) {
+            case "express" :
+                layanan = 0.2;
+                break;
+            case "regular":
+                layanan = 0.1;
+                break;
+            case "standard":
+                layanan = 0.05;
+                break;
+            default:
+                System.out.println("Maaf, jenis layanan tidak valid");
+            
         }
-
-        // Langkah 3: Output
-        System.out.println("Biaya pengiriman: $" + biayaPengiriman);
-
-        input.close();
+        
+        biayaPengiriman = beratPaket*jarakPengiriman*layanan*1000;
+        
+        System.out.println("Biaya pengiriman : Rp" +biayaPengiriman);
     }
 }
